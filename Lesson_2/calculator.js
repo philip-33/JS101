0d46030075579ecfad253f8d1bee4cd646e3c7b0
dev <22357184+philip-33@users.noreply.github.com>
2020-02-11 15:02:40 -0800

    Added Japanese language support

--- Lesson_2/calculator.js
+++ Lesson_2/calculator.js
@@ -1,7 +1,13 @@
 const readline = require("readline-sync");
-const PROMPTS = require("./calculator_messages.json");
+const MESSAGES = require("./calculator_messages.json");
+const LANGUAGE = "jp"; //use "en" for english and "jp" for japanese
 
-function prompt(message) {
+function messages(message, lang = "en") {
+  return MESSAGES[lang][message];
+}
+
+function prompt(key) {
+  let message = messages(key, LANGUAGE);
   console.log(`=> ${message}`);
 }
 
@@ -9,31 +15,31 @@ function invalidNumber(number) {
   return number.trimStart() === "" || Number.isNaN(Number(number));
 }
 
-prompt(PROMPTS.greeting);
+prompt("greeting");
 let keepCalculating = true;
 
 while (keepCalculating) {
-  prompt(PROMPTS.first);
+  prompt("first");
   let number1 = readline.question();
 
   while (invalidNumber(number1)) {
-    prompt(PROMPTS.number_fail);
+    prompt("number_fail");
     number1 = readline.question();
   }
 
-  console.log(PROMPTS.second);
+  prompt("second");
   let number2 = readline.question();
 
   while (invalidNumber(number2)) {
-    prompt(PROMPTS.number_fail);
+    prompt("number_fail");
     number2 = readline.question();
   }
 
-  prompt(PROMPTS.calculation);
+  prompt("calculation");
   let operation = readline.question();
 
   while (!["1", "2", "3", "4"].includes(operation)) {
-    prompt(PROMPTS.calculation_fail);
+    prompt("calculation_fail");
     operation = readline.question();
   }
 
@@ -52,8 +58,9 @@ while (keepCalculating) {
       output = Number(number1) / Number(number2);
       break;
   }
-  console.log(`The result is: ${output}`);
+  prompt("result");
+  console.log(output);
 
-  prompt(PROMPTS.continue);
+  prompt("continue");
   keepCalculating = readline.question().toLowerCase() === "yes";
 }
