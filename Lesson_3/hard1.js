2b87906249c6fc3f1c111276a5cbdc953096224b
dev <22357184+philip-33@users.noreply.github.com>
2020-02-27 21:41:35 -0800

    Disabled eslint and prettier for Lesson 3 - Hard 1

---
+++ Lesson_3/hard1.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+// Question 1
+/*
+Will the following functions return the same results?
+*/
+
+/*
+es lint reports that the object inside second() is 
+*/
+function first() {
+  return {
+    prop1: "hi there"
+  };
+}
+
+function second() {
+  return
+  {
+    prop1: "hi there"
+  };
+}
+
+console.log(first());
+console.log(second());
+
+// Question 2
+/*
+
+*/
+
+/*
+
+*/
+
+// Question 3
+/*
+
+*/
+
+/*
+
+*/
+
+// Question 4
+/*
+
+*/
+
+/*
+
+*/
