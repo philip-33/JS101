df0aa4fd700eb90359154cbf4a042b3cfb24e09c
dev <22357184+philip-33@users.noreply.github.com>
2020-02-29 09:59:10 -0800

    File cleanup

--- lesson_4/01_basics.js
+++
@@ -1,5 +0,0 @@
-/*
-Collections are made up of individual elements.
-To work with collections, we need to understand how they are structured
-nd how to reference and assign the individual elements within them.
-*/
