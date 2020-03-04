41d8db7d8ecc318d59a477058a933e3e9b8348a1
dev <22357184+philip-33@users.noreply.github.com>
2020-03-04 13:36:34 -0800

    Lesson 4 practice problems

---
+++ lesson_4/practice09.js
@@ -0,0 +1,20 @@
+/*
+Add up all of the ages from the Munster family object:
+
+input: object with names and ages
+output: interger of total of ages
+
+*/
+
+let ages = {
+  Herman: 32,
+  Lily: 30,
+  Grandpa: 5843,
+  Eddie: 10,
+  Marilyn: 22,
+  Spot: 237
+};
+
+let totalAge = Object.values(ages).reduce((total, age) => total + age, 0);
+
+console.log(totalAge);
