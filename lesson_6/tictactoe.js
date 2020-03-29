27ef32afcc5096693845fe4dcfc9f6a334a690a1
dev <22357184+philip-33@users.noreply.github.com>
2020-03-29 02:16:21 -0700

    Added computer offense and defense to Tic Tac Toe

--- lesson_6/tictactoe.js
+++ lesson_6/tictactoe.js
@@ -1,5 +1,25 @@
 /* eslint-disable max-statements */
 /* eslint-disable max-lines-per-function */
+/*
+defense + offense
+a defensive square is when the other player has 2/3
+an offensive square is where the computer has 2/3
+also, if the middle square is not take, use that first.
+
+function computerTurnAI(board){
+  if (checkCenterSquare(board)) return 5;
+
+  if findAtRiskSquare(Board, player) {
+    return findAtRiskSquare(Board, player);
+  }
+
+  if findAtRiskSquare(Board, computer) {
+    return findAtRiskSquare(Board, computer);
+  }
+
+  return Math.floor(Math.random * 9);
+}
+*/
 const readline = require('readline-sync');
 
 const MAX_WINS = 5;
@@ -7,6 +27,12 @@ const MAX_WINS = 5;
 const INITIAL_MARKER = ' ';
 const HUMAN_MARKER = 'O';
 const COMPUTER_MARKER = 'X';
+
+//prettier-ignore
+const WINNING_LINES = [ [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+                        [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+                        [1, 5, 9], [3, 5, 7] // diagonals
+];
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
@@ -65,9 +91,8 @@ function playerChoosesSquare(board) {
 }
 
 function computerChoosesSquare(board) {
-  let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
-  let square = emptySquares(board)[randomIndex];
-  board[square] = COMPUTER_MARKER;
+  let computersMove = computerTurnAI(board);
+  board[computersMove] = COMPUTER_MARKER;
 }
 
 function boardFull(board) {
@@ -79,15 +104,8 @@ function someoneWon(board) {
 }
 
 function detectWinner(board) {
-  //prettier-ignore
-  let winningLines = [
-    [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
-    [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
-    [1, 5, 9], [3, 5, 7]             // diagonals
-  ];
-
-  for (let line = 0; line < winningLines.length; line++) {
-    let [sq1, sq2, sq3] = winningLines[line];
+  for (let line = 0; line < WINNING_LINES.length; line++) {
+    let [sq1, sq2, sq3] = WINNING_LINES[line];
 
     if (
       board[sq1] === HUMAN_MARKER &&
@@ -133,6 +151,66 @@ function displayTournamentScore(scoreObject) {
   prompt(`Computer Score: ${scoreObject.computer}`);
 }
 
+function checkCenterSquare(board) {
+  if (board['5'] === INITIAL_MARKER && emptySquares(board).includes('5')) {
+    return '5';
+  }
+  return null;
+}
+
+function findAtRiskSquare(board, marker) {
+  /*
+  initialize a variable to store the 'at risk' space
+  step through every winning array
+    initialize/reset counters for marker and INITAL_MARKER
+    step through each element in the winning line array
+      check the board at the current element
+        if marker, increment marker counts
+        if empty, increment empty counter and flag a potential move
+    iff 2 markers and 1 empty element were found,
+    and the board is empty on the space that was found,
+      set the 'at risk' space to be the potential space
+  */
+  let atRiskSquare = null;
+
+  WINNING_LINES.forEach(line => {
+    let markerCount = 0;
+    let emptyCount = 0;
+    let potentialSquare = '';
+
+    line.forEach(square => {
+      if (board[square] === marker) {
+        markerCount++;
+      }
+      if (board[square] === INITIAL_MARKER) {
+        emptyCount++;
+        potentialSquare = String(square);
+      }
+    });
+
+    if (
+      markerCount === 2 &&
+      emptyCount === 1 &&
+      emptySquares(board).includes(potentialSquare)
+    ) {
+      atRiskSquare = potentialSquare;
+    }
+  });
+  return atRiskSquare;
+}
+
+function computerTurnAI(board) {
+  if (checkCenterSquare(board)) {
+    return '5';
+  } else if (findAtRiskSquare(board, HUMAN_MARKER)) {
+    return findAtRiskSquare(board, HUMAN_MARKER);
+  } else if (findAtRiskSquare(board, COMPUTER_MARKER)) {
+    return findAtRiskSquare(board, COMPUTER_MARKER);
+  }
+  let randomValue = Math.floor(Math.random() * emptySquares(board).length);
+  return emptySquares(board)[randomValue];
+}
+
 while (true) {
   let scores = {
     player: 0,
@@ -167,12 +245,12 @@ while (true) {
       break;
     }
 
-    prompt('Continue the Tournament? (n to quit)');
+    prompt('Continue the tournament? (n to quit)');
     let answer = readline.question().toLowerCase()[0];
     if (answer === 'n') break;
   }
   displayTournamentScore(scores);
-  prompt('Play another Tournament? (n to quit)');
+  prompt('Play another tournament? (n to quit)');
   let answer = readline.question().toLowerCase()[0];
   if (answer === 'n') break;
 }
