cf94b78752763addb36b60fff3dbb807451da18c
dev <22357184+philip-33@users.noreply.github.com>
2020-04-02 23:42:11 -0700

    Added multiple bonus features to tictactoe. Also included a logic diagram.

--- lesson_6/tictactoe.js
+++ lesson_6/tictactoe.js
@@ -1,25 +1,41 @@
 /* eslint-disable max-statements */
 /* eslint-disable max-lines-per-function */
-/*
-defense + offense
-a defensive square is when the other player has 2/3
-an offensive square is where the computer has 2/3
-also, if the middle square is not take, use that first.
+/* Launch School Lesson 6: Tic Tac Toe
 
-function computerTurnAI(board){
-  if (checkCenterSquare(board)) return 5;
+  This program includes the following features:
+  - Improved "join" for player square selection:
+      The list of available squares uses oxford commas
+  - Score tracking over 5 games:
+      This feature incorporates a first-to-5 scoring structure.
+      An object was implemented in the main game loop to hold the relevant data
+  - Defensive and Offensive AI:
+      The computer will block player wins if it can't win immediately.
+  - Refined computer turns:
+      The computer will use its AI and go for the middle square if available
+  - Set the first player or prompt for choice
+      If the FIRST_PLAYER constant is set to 'choose', the program will prompt
+      for a new first player at the beginning of every tournament.
+      Added this functionality to the game object
+  - Improved play-again handling:
+      The LS instructions state to only accept Y, y, N, or n as
+      input, but this is poor UX design. The version here will
+      continue with the next match or another tournament if
+      anything *except* n is provided. This makes the logic
+      and the check itself easier to understand.
+  - Improved game loop:
+      refactored player and computer moves into a single function,
+      tracking the turns with the game object.
+  - Implemented a dashboard:
+      Included a block of game stats when the board is drawn
+      - Markers for the player and computer
+      - Current tournament scores
+      - Identify the current player
+  - Screen clear toggle:
+      Added a constant that, if set to true, will disable the screen clearing.
+      (Very helpful in debugging)
 
-  if findAtRiskSquare(Board, player) {
-    return findAtRiskSquare(Board, player);
-  }
-
-  if findAtRiskSquare(Board, computer) {
-    return findAtRiskSquare(Board, computer);
-  }
-
-  return Math.floor(Math.random * 9);
-}
 */
+
 const readline = require('readline-sync');
 
 const MAX_WINS = 5;
@@ -27,24 +43,28 @@ const MAX_WINS = 5;
 const INITIAL_MARKER = ' ';
 const HUMAN_MARKER = 'O';
 const COMPUTER_MARKER = 'X';
+const FIRST_PLAYER = 'choose'; // valid options: 'player' 'computer' 'choose'
+
+const DISABLE_SCREEN_CLEAR = false;
 
 //prettier-ignore
 const WINNING_LINES = [ [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
-                        [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
-                        [1, 5, 9], [3, 5, 7] // diagonals
+                      [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+                      [1, 5, 9], [3, 5, 7] // diagonals
 ];
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
-function displayBoard(board, scores) {
-  console.clear();
+function displayBoard(board, gameObj) {
+  if (!DISABLE_SCREEN_CLEAR) console.clear();
 
-  console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}`);
+  console.log(`You are '${HUMAN_MARKER}'. Computer is '${COMPUTER_MARKER}'.`);
   console.log(
-    `The current score is Player: ${scores.player} Computer: ${scores.computer}`
+    `The current score is Player: ${gameObj.playerScore} Computer: ${gameObj.computerScore}`
   );
+  console.log(`It is the ${gameObj.currentPlayer}'s turn.`);
 
   console.log('');
   console.log('     |     |');
@@ -128,9 +148,7 @@ function detectWinner(board) {
 function joinOr(contentArray, delimiter = ', ', linkWord = 'or') {
   if (contentArray.length === 1) return contentArray[0];
   if (contentArray.length === 2) {
-    return `${contentArray[0]} 
-            ${linkWord} 
-            ${contentArray[1]}`;
+    return `${contentArray[0]} ${linkWord} ${contentArray[1]}`;
   } else {
     contentArray[contentArray.length - 1] = `${linkWord} ${
       contentArray[contentArray.length - 1]
@@ -141,14 +159,14 @@ function joinOr(contentArray, delimiter = ', ', linkWord = 'or') {
 }
 
 function detectTournamentWinner(scoreObject) {
-  if (scoreObject.player === MAX_WINS) return 'Player';
-  if (scoreObject.computer === MAX_WINS) return 'Computer';
+  if (scoreObject.playerScore === MAX_WINS) return 'Player';
+  if (scoreObject.computerScore === MAX_WINS) return 'Computer';
   return null;
 }
 
 function displayTournamentScore(scoreObject) {
-  prompt(`Player Score: ${scoreObject.player}`);
-  prompt(`Computer Score: ${scoreObject.computer}`);
+  prompt(`Player Score: ${scoreObject.playerScore}`);
+  prompt(`Computer Score: ${scoreObject.computerScore}`);
 }
 
 function checkCenterSquare(board) {
@@ -160,7 +178,7 @@ function checkCenterSquare(board) {
 
 function findAtRiskSquare(board, marker) {
   /*
-  initialize a variable to store the 'at risk' space
+  initialize a variable to store the 'at risk' space to null
   step through every winning array
     initialize/reset counters for marker and INITAL_MARKER
     step through each element in the winning line array
@@ -170,6 +188,7 @@ function findAtRiskSquare(board, marker) {
     iff 2 markers and 1 empty element were found,
     and the board is empty on the space that was found,
       set the 'at risk' space to be the potential space
+  the function returns null unless an at risk space was found.
   */
   let atRiskSquare = null;
 
@@ -199,6 +218,14 @@ function findAtRiskSquare(board, marker) {
   return atRiskSquare;
 }
 
+function moveForCurrentPlayer(board, currentPlayer) {
+  if (currentPlayer === 'player') {
+    playerChoosesSquare(board);
+  } else {
+    computerChoosesSquare(board);
+  }
+}
+
 function computerTurnAI(board) {
   if (checkCenterSquare(board)) {
     return '5';
@@ -211,47 +238,88 @@ function computerTurnAI(board) {
   return emptySquares(board)[randomValue];
 }
 
-while (true) {
-  let scores = {
-    player: 0,
-    computer: 0
-  };
+function swapCurrentPlayer(currentPlayer) {
+  let nextPlayer;
 
+  if (currentPlayer === 'player') {
+    nextPlayer = 'computer';
+  } else {
+    nextPlayer = 'player';
+  }
+
+  return nextPlayer;
+}
+
+function configurePlayer(newFirstPlayer) {
+  let playerChoice = undefined;
+  if (newFirstPlayer === 'choose') {
+    do {
+      prompt(
+        `Press '1' for the human player to go first, or press '2' for the computer player to go first.`
+      );
+      playerChoice = readline.question();
+    } while (!playerChoice.match(/^[12]/));
+
+    if (playerChoice === '1') return 'player';
+    if (playerChoice === '2') return 'computer';
+  } else {
+    playerChoice = newFirstPlayer;
+  }
+
+  return playerChoice;
+}
+
+function tictactoeMainLoop() {
   while (true) {
-    let board = initializeBoard();
+    let gameObject = {
+      playerScore: 0,
+      computerScore: 0,
+      currentPlayer: 'computer',
+      firstPlayer: 'computer'
+    };
+
+    gameObject.firstPlayer = configurePlayer(FIRST_PLAYER);
+    gameObject.currentPlayer = gameObject.firstPlayer;
 
     while (true) {
-      computerChoosesSquare(board);
-      if (someoneWon(board) || boardFull(board)) break;
+      let board = initializeBoard();
 
-      displayBoard(board, scores);
+      while (true) {
+        displayBoard(board, gameObject);
+        moveForCurrentPlayer(board, gameObject.currentPlayer);
+        if (someoneWon(board) || boardFull(board)) break;
+        displayBoard(board, gameObject);
+        gameObject.currentPlayer = swapCurrentPlayer(gameObject.currentPlayer);
+      }
 
-      playerChoosesSquare(board);
-      if (someoneWon(board) || boardFull(board)) break;
+      displayBoard(board, gameObject);
+      gameObject.currentPlayer = gameObject.firstPlayer;
+
+      if (someoneWon(board)) {
+        prompt(`${detectWinner(board)} won the match!`);
+        if (detectWinner(board) === 'Player') gameObject.playerScore++;
+        if (detectWinner(board) === 'Computer') gameObject.computerScore++;
+      } else {
+        prompt("It's a tie!");
+      }
+
+      if (detectTournamentWinner(gameObject)) {
+        prompt(`${detectTournamentWinner(gameObject)} wins the Tournament!`);
+        break;
+      }
+
+      prompt('Continue the tournament? (n to quit)');
+      let answer = readline.question().toLowerCase()[0];
+      if (answer === 'n') break;
     }
 
-    displayBoard(board, scores);
-
-    if (someoneWon(board)) {
-      prompt(`${detectWinner(board)} won the match!`);
-      if (detectWinner(board) === 'Player') scores.player++;
-      if (detectWinner(board) === 'Computer') scores.computer++;
-    } else {
-      prompt("It's a tie!");
-    }
-
-    if (detectTournamentWinner(scores)) {
-      prompt(`${detectTournamentWinner(scores)} wins the Tournament!`);
-      break;
-    }
-
-    prompt('Continue the tournament? (n to quit)');
+    displayTournamentScore(gameObject);
+    prompt('Play another Tournament? (n to quit)');
     let answer = readline.question().toLowerCase()[0];
     if (answer === 'n') break;
   }
-  displayTournamentScore(scores);
-  prompt('Play another tournament? (n to quit)');
-  let answer = readline.question().toLowerCase()[0];
-  if (answer === 'n') break;
+
+  prompt('Thanks for playing Tic Tac Toe!');
 }
-prompt('Thanks for playing Tic Tac Toe!');
+
+tictactoeMainLoop();
