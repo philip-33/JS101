c922eea9dbec6f57d6bde1aa20affee314f12a98
dev <22357184+philip-33@users.noreply.github.com>
2020-04-09 00:07:51 -0700

    Added features to Twenty One: dealer turn, hit on soft 17, correct hand values, hide one dealer card during player turn, a 'cut card' that is placed randomly near the back 1/3 of the deck

--- lesson_6/21.js
+++ lesson_6/21.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-statements */
 /* eslint-disable max-lines-per-function */
 /* eslint-disable no-mixed-operators */
 'use strict';
@@ -15,6 +16,8 @@ In addition to the Launch School requirements, the following has been added:
   shoe near the end (approx. 1/4 to 1/3 from the end). Once this plastic marker
   is drawn, the shoe is swapped out for a completely new set of decks in a
   different shoe.
+
+- The dealer will hit on a soft 17 (Ace + six)
 
 Deck: 260 cards (5 decks).
 
@@ -46,7 +49,8 @@ The winner has the higher total
 
 pseudocode:
 1. Init deck
-2. Deal cards
+2. Deal cards (first card goes to player, first dealer card is face down)
+              (initial cards are dealt to alternating players)
 3. Player turn: hit or stay
     -repeat until bust or stay
     -if player busts, dealer wins
@@ -68,11 +72,15 @@ If the card selected is not available, just draw another card.
 
 let seedrandom = require('seedrandom');
 const rng = seedrandom();
+const CUT_CARD_RATIO = 0.33;
+const CUT_CARD_VARIATION = 0.15;
 const CARD_ICON = '🂠';
-const CUT_CARD_SEED_RATIO = 33;
+const DISABLE_SCREEN_CLEAR = true;
 
 //prettier-ignore
 const DECK_INIT_ARRAY = ['A', 'A', 'A', 'A', 'A', '2', '2', '2', '2', '2', '3', '3', '3', '3', '3', '4', '4', '4', '4', '4', '5', '5', '5', '5', '5', '6', '6', '6', '6', '6', '7', '7', '7', '7', '7', '8', '8', '8', '8', '8', '9', '9', '9', '9', '9', '10', '10', '10', '10', '10', 'J', 'J', 'J', 'J', 'J', 'Q', 'Q', 'Q', 'Q', 'Q', 'K', 'K', 'K', 'K', 'K'];
+
+const CARD_VALUE_INDEX = [0, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
@@ -85,7 +93,7 @@ function initializeDeck() {
   */
   let newDeck = [];
 
-  DECK_INIT_ARRAY.forEach(card => {
+  DECK_INIT_ARRAY.forEach((card) => {
     newDeck.push(card);
   });
 
@@ -96,7 +104,7 @@ function totalRemainingCards(shoe) {
   let allCardsArray = Object.values(shoe);
 
   let totalCount = allCardsArray
-    .map(arr => {
+    .map((arr) => {
       return arr.length;
     })
     .reduce((acc, cur) => {
@@ -153,6 +161,60 @@ function cardIsPresent(suit, value, shoe) {
   return false;
 }
 
+function handHasAces(cardArray) {
+  /*
+    step through each element of the array, taking [idx][1]
+  */
+  let hasAces = false;
+  let foundAces = cardArray.filter((card) => {
+    return card[1] === 'A';
+  });
+
+  if (foundAces.length > 0) hasAces = true;
+
+  return hasAces;
+}
+
+function handHasBusted(cardArray) {
+  let busted = false;
+
+  if (calcHandValue(cardArray) === 0) busted = true;
+
+  return busted;
+}
+
+function calcHandValue(cardArray) {
+  /*
+    walk through the array, adding up all the values.
+      convert as necessary.
+    use a variable to track all found aces,
+    and before returning the final value,
+      convert enough aces from 11 to 1 so the hand is the best possible score.
+  */
+
+  let foundAceCount = 0;
+  let total = 0;
+  let valueArray = cardArray.map((card) => card[1]);
+
+  valueArray.forEach((value) => {
+    if (value === 'A') {
+      foundAceCount++;
+      total += 11;
+    } else if (value === 'J' || value === 'Q' || value === 'K') {
+      total += 10;
+    } else {
+      total += Number(value);
+    }
+  });
+
+  while (foundAceCount > 0 && total > 21) {
+    total -= 10;
+    foundAceCount--;
+  }
+
+  return total;
+}
+
 function drawCardFromShoe(shoe) {
   let suit;
   let value;
@@ -167,7 +229,94 @@ function drawCardFromShoe(shoe) {
     }
   } while (true);
 
-  return suit + value;
+  return [suit, value];
+}
+
+function displayHand(cardArray) {
+  /*
+    step through each element of the array,
+      append to string in this format:
+        first element (no space) second element (space)
+  */
+  let hand = '';
+  cardArray.forEach((card) => {
+    hand += card[0] + card[1] + ' ';
+  });
+  return hand;
+}
+
+function displayCardTable(playerCards, houseCards, dealerCardIsFaceDown) {
+  /*
+  check the state of the dealer's face card.
+  if hidden:
+    show the dealer's hand with one card hidden,
+    display the score of the single card
+  if not hidden:
+    show the dealer's whole hand
+    display the total score
+  */
+  if (!DISABLE_SCREEN_CLEAR) console.clear();
+  if (dealerCardIsFaceDown) {
+    console.log(
+      `\nThe House's hand: ${houseCards[0][0]}${houseCards[0][1]} ${CARD_ICON}`
+    );
+    console.log(
+      `The House's hand is showing ${calcHandValue([houseCards[0]])}`
+    );
+  } else {
+    console.log(`\nThe House's hand: ${displayHand(houseCards)}`);
+    console.log(`The House's hand value is ${calcHandValue(houseCards)}`);
+  }
+
+  console.log(`\nThe Player's hand: ${displayHand(playerCards)}`);
+  console.log(`The Player's hand value is ${calcHandValue(playerCards)}`);
+}
+
+function generateCutCardIndex(shoe) {
+  /*
+    get total cards
+    multiply by const rate
+    add (or subtract a random number of cards),
+    verify this number is less than the total cards available
+    return the value to be used to emulate the cut card
+  */
+  let totalCards = totalRemainingCards(shoe);
+  let variationRatio = totalCards * CUT_CARD_VARIATION;
+  let additionalCards = Math.floor(rng() * variationRatio - variationRatio / 2);
+
+  return Math.floor(CUT_CARD_RATIO * totalCards + additionalCards);
+}
+
+function hit(cardArray, shoe) {}
+
+function houseTurn(houseCards, shoe) {
+  /*
+    hit until value is 17 or greater.
+    if 17 with aces, hit,
+    else if <= 17, hit,
+    else, stay
+    handHasAces(houseCards)
+  */
+
+  while (
+    calcHandValue(houseCards) < 17 &&
+    handHasBusted(houseCards) === false
+  ) {
+    houseCards.push(drawCardFromShoe(shoe));
+  }
+
+  if (handHasAces(houseCards) && calcHandValue(houseCards) === 17) {
+    houseCards.push(drawCardFromShoe(shoe));
+  }
+
+  return houseCards;
+}
+
+function playerTurn(playerCards, shoe) {
+  /*
+    if the total is less than 21, ask to stay or hit
+    on stay, the function is done
+  */
 }
 
 function mainGameLoop() {
@@ -177,15 +326,41 @@ function mainGameLoop() {
     shoe['♥️'] = initializeDeck();
     shoe['♦️'] = initializeDeck();
     shoe['♣️'] = initializeDeck();
-    let cutCardIndex = totalRemainingCards(shoe) / CUT_CARD_SEED_RATIO;
+    let cutCard = generateCutCardIndex(shoe);
 
-    while (totalRemainingCards(shoe) > cutCardIndex) {
-      for (let idx = 0; idx < 210; idx++) {
-        console.log(`${drawCardFromShoe(shoe)} was drawn from the shoe.`);
-        console.log(`There are ${totalRemainingCards(shoe)} cards remaining`);
-      }
-      break;
+    while (totalRemainingCards(shoe) > cutCard) {
+      console.log(`\n=== A new hand is being dealt! ===`);
+      let playerCards = [];
+      let houseCards = [];
+      let dealerCardIsFaceDown = true;
+
+      playerCards.push(drawCardFromShoe(shoe));
+      houseCards.push(drawCardFromShoe(shoe));
+      playerCards.push(drawCardFromShoe(shoe));
+      houseCards.push(drawCardFromShoe(shoe));
+      displayCardTable(playerCards, houseCards, dealerCardIsFaceDown);
+
+      // player turn
+      displayCardTable(playerCards, houseCards, dealerCardIsFaceDown);
+
+      dealerCardIsFaceDown = false;
+      houseCards = houseTurn(houseCards, shoe); // dealer turn
+      displayCardTable(playerCards, houseCards, dealerCardIsFaceDown);
+      // detect winner
+
+      /*
+      this game is not back and forth like tic tac toe, it's first and second.
+      Both players could lose until the final decision
+
+      draw cards for both players (alternating). Second dealer card is face down
+      The dealer's card does not go face up until the dealer takes their turn.
+
+      player goes through actions until stay or bust, each new card is added
+      dealer card goes face up
+      computer goes through actions until stay or bust, each new card is added
+      */
     }
+
     prompt(`The shoe is too small, and will be refreshed.`);
     break;
   }
